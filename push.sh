2a31f4a1094c9239f763305be87690649306f2c1
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-01-11 17:31:31 +0000

    required 6

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "required 5"
+git commit -m "required 6"
 git push
